ff18c83d2cd5cc40c3d27e6e2de60b8f5ba1b862
David Blaise <dblaise@checkintocash.com>
2018-09-25 07:45:23 -0400

    not sure what else to do

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,3 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-  if ('serviceWorker' in navigator && environment.production) {
-    navigator.serviceWorker.register('ngsw-worker.js');
-  }
-}).catch(err => console.log(err));
